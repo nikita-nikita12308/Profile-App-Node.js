966f7be455017e551351f1d7fb5e5088944b40ea
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-02-09 22:01:35 +0200

    styles

--- login_app/client/src/components/Username.js
+++ login_app/client/src/components/Username.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from '../styles/Username.module.css'
 
 export default function Username() {
     return (
         <div className="container mx-auto">
             <div className='flex justify-center items-center h-screen'>
-                <div>
+                <div className={styles.glass}>
                     <div className="title flex flex-col items-center">
                         <h4 className="text-5xl font-bold">Hello again</h4>
                         <span className="py-4 text-xl w-2/3 text-center text-gray-500">
@@ -14,11 +15,11 @@ export default function Username() {
 
                     <form className='py-1'>
                         <div className="profile flex justify-center py-4">
-                            <img src="" alt="avatar"/>
+                            <img src="" className={styles.profile_img} alt="avatar"/>
                         </div>
                         <div className="textbox flex flex-col items-center">
-                            <input type="text" placeholder='Username'/>
-                            <button type='submit'>Let's go</button>
+                            <input className={styles.textbox} type="text" placeholder='Username'/>
+                            <button className={styles.btn} type='submit'>Let's go</button>
                         </div>
 
                         <div className="text-center py-4">
